f1732a41e81e9fdcd55c6836d93a63ff6704a9d3
Hussein Issa <www.hussein2001@gmail.com>
2024-05-26 18:24:55 +0300

    add floating a ction button to trigger plugin

--- lib/views/questions_view.dart
+++ lib/views/questions_view.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:questions_app/widgets/custom_app_bar.dart';
 import 'package:questions_app/widgets/questions_view_body.dart';
+import 'package:show_toast_when_fetch_data_api/show_toast_when_fetch_data_api_method_channel.dart';
 
 class QuestionsView extends StatelessWidget {
   const QuestionsView({super.key});
   static String id = 'QuestionsView';
   @override
   Widget build(BuildContext context) {
-    return const  Scaffold(
-        body: Column(
-      children: [
-         CustomAppBar(),
-         Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
-          child: Text(
-            'All Questions',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
+    return Scaffold(
+      body: const Column(
+        children: [
+          CustomAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              'All Questions',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
             ),
           ),
-        ),
-         QuestionsViewBody(),
-      ],
-    ));
+          QuestionsViewBody(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await MethodChannelShowToastWhenFetchDataApi().getDataApi();
+        },
+        child: const Text('Plugin'),
+      ),
+    );
   }
 }
