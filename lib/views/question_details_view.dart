6b27bcf17efb057734dc64debe4dff3735c113a4
Hussein Issa <www.hussein2001@gmail.com>
2024-05-26 21:35:51 +0300

    display body question in question details view

--- lib/views/question_details_view.dart
+++ lib/views/question_details_view.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:questions_app/models/question_model.dart';
 import 'package:questions_app/widgets/custom_app_bar.dart';
 import 'package:questions_app/widgets/custom_card.dart';
 import 'package:questions_app/widgets/title_view.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class QuestionDetailsView extends StatefulWidget {
   const QuestionDetailsView({super.key});
@@ -21,15 +24,37 @@ class _QuestionDetailsViewState extends State<QuestionDetailsView> {
   @override
   Widget build(BuildContext context) {
     question = ModalRoute.of(context)!.settings.arguments as QuestionModel;
-    return  Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-           const CustomAppBar(),
-           const TitleView(
-            title: 'Question Details',
-          ),
+          const CustomAppBar(),
           CustomCard(
             question: question,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const TitleView(
+                      title: 'Question Details',
+                    ),
+                    Divider(),
+                    HtmlWidget(
+                      question.body,
+                      textStyle: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Divider(),
+                    const TitleView(
+                      title: 'Answers',
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
